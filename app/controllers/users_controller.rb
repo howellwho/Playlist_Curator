a395e322653a4fbc3a6c550b025d0810ee78412e
howellwho <howellwho@gmail.com>
2016-02-25 17:12:12 -0800

    redirect issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,6 @@ class UsersController < ApplicationController
 
   def spotify_auth
     # redirect_to "https://accounts.spotify.com/authorize" + "?client_id=1346712e0b264189aac468ff626623e6&response_type=code&redirect_uri=http:127.0.0.1:3000/auth/spotify/callback"
-    redirect_to "https://accounts.spotify.com/authorize/?client_id=#{ENV['SPOTIFY_CLIENT_ID']}&response_type=code&redirect_uri=#{request.original_url}/auth/spotify/callback"
+    redirect_to "https://accounts.spotify.com/authorize/?client_id=#{ENV['SPOTIFY_CLIENT_ID']}&response_type=code&redirect_uri=#{request.original_url}"
   end
 end
